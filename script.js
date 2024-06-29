4a5ff9d3d25c5cfc05b671323eab7a1d31f0d725
hcinfo9 <hcinformatica9438@gmail.com>
2024-06-29 08:03:14 -0300

    ajuste na resposividade

--- script.js
+++ script.js
@@ -1,19 +1,60 @@
-let swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
+let button_menu = document.querySelector('.button-menu')
+let nav_menu= document.querySelector('.menu-navbar')
+let links = document.querySelectorAll('.menu-navbar ul li a')
+
+
+let swiper = createSwiper(".mySwiper", ".swiper-pagination", ".swiper-button-next", ".swiper-button-prev");
+let header = document.querySelector('header');
+
+function createSwiper(container, pagination, nextButton, prevButton) {
+  return new Swiper(container, {
+    slidesPerView: handleWidth(),
     spaceBetween: 30,
     pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
+      el: pagination,
+      clickable: true,
     },
-    navigation:{
-        nextEl: '.swiper-button-next',
-        prevEl: ".swiper-button-prev",
+    navigation: {
+      nextEl: nextButton,
+      prevEl: prevButton,
     },
+  });
+}
+
+
+function handleWidth(){
+    let getWidth = window.innerWidth || document.documentElement.clientWidth
+    let slideShow = 3
+
+    if(getWidth < 1001) {
+        slideShow = 2;
+    }
+
+    if(getWidth < 700) {
+        slideShow = 1
+    }
+    
+    
+    return slideShow
+}
+
+button_menu.addEventListener('click', () => {
+ 
+    nav_menu.classList.toggle('active')
+})
+
+
+links.forEach(element => {
+    element.addEventListener('click', () =>{
+        nav_menu.classList.toggle('active')
+    })
 });
 
+window.addEventListener('resize', () => {
+    swiper.params.slidesPerView = handleWidth();
+    swiper.update();
+})
 
-
-let header = document.querySelector('header')
 
 window.addEventListener("scroll", () => {
     if(window.scrollY >=200) {
@@ -24,4 +65,5 @@ window.addEventListener("scroll", () => {
           header.style.background="transparent"
       }
   })
+
 
